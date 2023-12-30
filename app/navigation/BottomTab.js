593bc3621ddd3979a31424b951d438f1e62a4f3d
ankitmahotla <ankitmahotla45@gmail.com>
2023-12-30 19:55:21 +0530

    Basic App Setup

---
+++ app/navigation/BottomTab.js
@@ -0,0 +1,53 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../screens/Home";
+import Search from "../screens/Search";
+import Favorite from "../screens/Favorite";
+import Profile from "../screens/Profile";
+import { Ionicons } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+
+const BottomTab = () => {
+  return (
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={Search}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Favorite"
+        component={Favorite}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="heart" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default BottomTab;
---
+++ app/screens/Home.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import Header from "../components/home/Header";
+
+const Home = () => {
+  return (
+    <View>
+      <Header />
+    </View>
+  );
+};
+
+export default Home;
